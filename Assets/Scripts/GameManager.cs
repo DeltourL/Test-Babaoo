54e80a09e38303490c534474b991b13765a51003
Kiwaka <lucas.deltour95@gmail.com>
2022-10-10 09:06:32 +0200

    added game scene

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using SceneTransitionSystem;
+
+namespace TeasingGame
+{
+
+    public class GameManager : MonoBehaviour
+    {
+        public TeasingGameScene SceneForButton;
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void GoToHomeScene()
+        {
+            STSSceneManager.LoadScene(SceneForButton.ToString());
+        }
+    }
+}
