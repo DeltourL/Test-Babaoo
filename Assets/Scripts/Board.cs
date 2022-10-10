264cab3f8f57e495509a1541203145c0f23d4fb8
Kiwaka <lucas.deltour95@gmail.com>
2022-10-10 10:32:33 +0200

    board shuffling

---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,117 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+
+    private readonly int boardSize = 3;
+
+
+    [SerializeField]
+    private Texture puzzleImage;
+    [SerializeField]
+    private Shader shader;
+
+    // number of moves made to shuffle the board
+    private int shuffleMoves = 20;
+    private Vector2Int previousMove = Vector2Int.zero;
+    private Tile emptySpace;
+
+    private Tile[,] tiles;
+
+    private void Start()
+    {
+        tiles = new Tile[boardSize, boardSize];
+    }
+
+    public void CreateBoard()
+    {
+        for (int y = 0; y < boardSize; y++)
+        {
+            for (int x = 0; x < boardSize; x++)
+            {
+                // first tile position in bottom left, full board is centered on 0
+                Vector2 position = new Vector2((boardSize - 1) * -1 * 0.5f, (boardSize - 1) * -1 * 0.5f);
+
+                position += new Vector2(x, y);
+
+                // tile creation
+                Tile tile = GameObject.CreatePrimitive(PrimitiveType.Quad).AddComponent<Tile>();
+                tile.transform.position = position;
+                tile.transform.parent = transform;
+                tile.coordinates = new Vector2Int(x, y);
+
+                // setting up the image on the tile
+                tile.GetComponent<Renderer>().material = new Material(shader)
+                {
+                    mainTexture = puzzleImage,
+                    mainTextureOffset = new Vector2(1.0f / boardSize * x, 1.0f / boardSize * y),
+                    mainTextureScale = new Vector2(1.0f / boardSize, 1.0f / boardSize)
+                };
+
+                // removing the middle piece
+                if (x == Mathf.RoundToInt(boardSize / 2) && y == Mathf.RoundToInt(boardSize / 2))
+                {
+                    tile.gameObject.SetActive(false);
+                    emptySpace = tile;
+                }
+
+                tiles[x, y] = tile;
+            }
+        }
+        Shuffle();
+    }
+
+    private void Shuffle()
+    {
+        for (int i = 0; i < shuffleMoves; i++)
+        {
+            List<Vector2Int> possibleMoves = new List<Vector2Int>();
+
+            // add every possible moves
+            if (emptySpace.coordinates.x > 0)
+            {
+                possibleMoves.Add(new Vector2Int(-1, 0));
+            }
+            if (emptySpace.coordinates.x < boardSize - 1)
+            {
+                possibleMoves.Add(new Vector2Int(1, 0));
+            }
+            if (emptySpace.coordinates.y > 0)
+            {
+                possibleMoves.Add(new Vector2Int(0, -1));
+            }
+            if (emptySpace.coordinates.y < boardSize - 1)
+            {
+                possibleMoves.Add(new Vector2Int(0, 1));
+            }
+
+            // avoid back and forth movement during shuffle
+            possibleMoves.Remove(previousMove * -1);
+
+            // choose a random tile and move it
+            Vector2Int move = possibleMoves[UnityEngine.Random.Range(0, possibleMoves.Count)];
+            Vector2Int chosenTileCoordinates = move + emptySpace.coordinates;
+            MoveTile(tiles[chosenTileCoordinates.x, chosenTileCoordinates.y]);
+
+            previousMove = move;
+        }
+    }
+
+    // switch places between tile and empty space 
+    void MoveTile(Tile tile)
+    {
+        Vector2Int targetCoord = emptySpace.coordinates;
+        emptySpace.coordinates = tile.coordinates;
+        tile.coordinates = targetCoord;
+
+        tiles[emptySpace.coordinates.x, emptySpace.coordinates.y] = emptySpace;
+        tiles[targetCoord.x, targetCoord.y] = tile;
+
+        Vector3 targetPosition = emptySpace.transform.position;
+        emptySpace.transform.position = tile.transform.position;
+        tile.transform.position = targetPosition;
+    }
+}
