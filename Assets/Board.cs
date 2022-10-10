a5596f846021c9b2d27e6a9122c90d5f782fce4d
Kiwaka <lucas.deltour95@gmail.com>
2022-10-10 09:54:19 +0200

    inverted x & y

--- Assets/Board.cs
+++ Assets/Board.cs
@@ -9,9 +9,9 @@ public class Board : MonoBehaviour
 
     public void CreateBoard()
     {
-        for (int x = 0; x < boardSize; x++)
+        for (int y = 0; y < boardSize; y++)
         {
-            for (int y = 0; y < boardSize; y++)
+            for (int x = 0; x < boardSize; x++)
             {
                 // first tile position in bottom left, centered on 0
                 Vector2 position = new Vector2((boardSize - 1) * -1 * 0.5f, (boardSize - 1) * -1 * 0.5f);
